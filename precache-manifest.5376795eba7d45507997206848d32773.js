e856c3302774e5542ca72d10e6c655e78c453727
lokeswar2 <lokeswaran.it20@bitsathy.ac.in>
2023-03-06 11:34:06 +0530

    Updates

---
+++ precache-manifest.5376795eba7d45507997206848d32773.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa79da768fd0f87ced0098baf64adca",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e0f407345e608a79b37c",
+    "url": "/portfolio/static/css/main.6d7e7647.chunk.css"
+  },
+  {
+    "revision": "0742edb410670c085791",
+    "url": "/portfolio/static/js/2.6c8a95f8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.6c8a95f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0f407345e608a79b37c",
+    "url": "/portfolio/static/js/main.ed1c0cf5.chunk.js"
+  },
+  {
+    "revision": "eb0aafffcb038f123bf0",
+    "url": "/portfolio/static/js/runtime-main.255623fe.js"
+  },
+  {
+    "revision": "ce9b200daa51aac0489c5827587da778",
+    "url": "/portfolio/static/media/kannan.ce9b200d.jpg"
+  },
+  {
+    "revision": "5be6f8216fad0adde289c23cddad7675",
+    "url": "/portfolio/static/media/shareyourbyte.5be6f821.jpg"
+  },
+  {
+    "revision": "0ff958ed6b1fe978e8ef87863d5ad6e9",
+    "url": "/portfolio/static/media/wikipediasearch.0ff958ed.jpg"
+  }
+]);
